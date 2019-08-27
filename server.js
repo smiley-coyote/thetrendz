ca3c4ddf59b8e7a9ebb0ec2fa1a9f5f530e73336
smiley-coyote <nick.a.alex@gmail.com>
2019-08-27 15:21:40 -0400

    added scripts in package.json for heroku build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const googleTrends = require('google-trends-api');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -41,11 +42,14 @@ app.post('/api/search', (req, res) => {
 
 })
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
